54eba10f5f705f55a498c6c078441ab9b80f10a7
kd14 <kd14@ford.com>
2023-08-12 20:13:40 +0530

    final code

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 build:
-	env GOOS=linux go build -ldflags="-s -w" -o bin/main main.go
+	env GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o bin/main main.go
 
 deploy_prod: build
 	serverless deploy --stage prod
--- main.go
+++ main.go
@@ -1,17 +1,31 @@
 package main
 
 import (
+	"context"
 	"net/http"
 
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/gin-gonic/gin"
 )
+
+func init() {
+	lambda.Start(HandleRequest)
+}
 
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
-	  c.JSON(http.StatusOK, gin.H{
-		"message": "pong",
-	  })
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
-  }
+}
+
+func HandleRequest(ctx context.Context, name events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       "Welcome to go lang serverless api",
+	}, nil
+}
